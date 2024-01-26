7c684b3a3205897caed0e79ca050e614bb240810
Almhyar Zahra <almhyarzahra@gmail.com>
2024-01-26 08:40:53 -0600

    wallet

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -7,6 +7,7 @@ import 'package:sportk/model/schedule_and_results_model.dart';
 import 'package:sportk/network/api_service.dart';
 import 'package:sportk/network/api_url.dart';
 import 'package:sportk/providers/auth_provider.dart';
+import 'package:sportk/screens/wallet/wallet_screen.dart';
 import 'package:sportk/utils/app_constants.dart';
 import 'package:sportk/utils/base_extensions.dart';
 import 'package:sportk/utils/my_icons.dart';
@@ -51,116 +52,119 @@ class _HomeScreenState extends State<HomeScreen> {
     // final time = DateTime(2024, 01, 21).millisecondsSinceEpoch;
     // print("time:: $time");
     return Scaffold(
-      body: CustomFutureBuilder(
-        future: _scheduleAndResultsfuture,
-        onRetry: () {},
-        onComplete: ((context, snapshot) {
-          return CustomScrollView(
-            slivers: [
-              SliverAppBar(
-                pinned: true,
-                leading: IconButton(
-                  onPressed: () {},
-                  icon: const CustomSvg(MyIcons.menu),
-                ),
-                actions: [
-                  IconButton(
-                    onPressed: () {
-                      context.push(const NewsScreen());
-                    },
-                    icon: const CustomSvg(MyIcons.search),
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: const CustomSvg(MyIcons.calender),
-                  ),
-                  Switch(
-                    value: true,
-                    onChanged: (value) {},
-                  ),
-                ],
-              ),
-              SliverToBoxAdapter(
-                child: Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: Text(
-                    "Tuesday 12-12-2024",
-                    style: context.textTheme.labelMedium,
-                  ),
-                ),
-              ),
-              SliverPadding(
-                padding: const EdgeInsets.all(20),
-                sliver: SliverList.separated(
-                  itemCount: 3,
-                  separatorBuilder: (context, index) => const SizedBox(height: 5),
-                  itemBuilder: (context, index) {
-                    return Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+        body:
+            //  CustomFutureBuilder(
+            //   future: _scheduleAndResultsfuture,
+            //   onRetry: () {},
+            //   onComplete: ((context, snapshot) {
+            //     return
+            CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          pinned: true,
+          leading: IconButton(
+            onPressed: () {
+              context.push(const WalletScreen());
+            },
+            icon: const CustomSvg(MyIcons.menu),
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {
+                context.push(const NewsScreen());
+              },
+              icon: const CustomSvg(MyIcons.search),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const CustomSvg(MyIcons.calender),
+            ),
+            Switch(
+              value: true,
+              onChanged: (value) {},
+            ),
+          ],
+        ),
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Text(
+              "Tuesday 12-12-2024",
+              style: context.textTheme.labelMedium,
+            ),
+          ),
+        ),
+        SliverPadding(
+          padding: const EdgeInsets.all(20),
+          sliver: SliverList.separated(
+            itemCount: 3,
+            separatorBuilder: (context, index) => const SizedBox(height: 5),
+            itemBuilder: (context, index) {
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const LeagueTile(),
+                  Container(
+                    height: 65,
+                    decoration: BoxDecoration(
+                      color: context.colorPalette.blue1FC,
+                      borderRadius: BorderRadius.circular(MyTheme.radiusSecondary),
+                    ),
+                    margin: const EdgeInsets.symmetric(vertical: 5),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        const LeagueTile(),
-                        Container(
-                          height: 65,
-                          decoration: BoxDecoration(
-                            color: context.colorPalette.blue1FC,
-                            borderRadius: BorderRadius.circular(MyTheme.radiusSecondary),
-                          ),
-                          margin: const EdgeInsets.symmetric(vertical: 5),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              const Spacer(),
-                              const Text("Madrid"),
-                              const CustomNetworkImage(
-                                kFakeImage,
-                                radius: 0,
-                                width: 25,
-                                height: 25,
-                                margin: EdgeInsetsDirectional.only(start: 10, end: 6),
+                        const Spacer(),
+                        const Text("Madrid"),
+                        const CustomNetworkImage(
+                          kFakeImage,
+                          radius: 0,
+                          width: 25,
+                          height: 25,
+                          margin: EdgeInsetsDirectional.only(start: 10, end: 6),
+                        ),
+                        const Text("2"),
+                        const SizedBox(width: 35),
+                        const Text("2"),
+                        const CustomNetworkImage(
+                          kFakeImage,
+                          radius: 0,
+                          width: 25,
+                          height: 25,
+                          margin: EdgeInsetsDirectional.only(start: 10, end: 6),
+                        ),
+                        const Text("Madrid"),
+                        const Spacer(),
+                        Transform.rotate(
+                          angle: -pi / 2,
+                          child: Container(
+                            height: 20,
+                            width: 40,
+                            padding: EdgeInsets.all(3),
+                            decoration: BoxDecoration(
+                              color: context.colorPalette.red000,
+                              borderRadius: BorderRadius.circular(MyTheme.radiusSecondary),
+                            ),
+                            alignment: Alignment.center,
+                            child: Text(
+                              "Live",
+                              style: context.textTheme.labelSmall!.copyWith(
+                                color: context.colorPalette.white,
                               ),
-                              const Text("2"),
-                              const SizedBox(width: 35),
-                              const Text("2"),
-                              const CustomNetworkImage(
-                                kFakeImage,
-                                radius: 0,
-                                width: 25,
-                                height: 25,
-                                margin: EdgeInsetsDirectional.only(start: 10, end: 6),
-                              ),
-                              const Text("Madrid"),
-                              const Spacer(),
-                              Transform.rotate(
-                                angle: -pi / 2,
-                                child: Container(
-                                  height: 20,
-                                  width: 40,
-                                  padding: EdgeInsets.all(3),
-                                  decoration: BoxDecoration(
-                                    color: context.colorPalette.red000,
-                                    borderRadius: BorderRadius.circular(MyTheme.radiusSecondary),
-                                  ),
-                                  alignment: Alignment.center,
-                                  child: Text(
-                                    "Live",
-                                    style: context.textTheme.labelSmall!.copyWith(
-                                      color: context.colorPalette.white,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ],
+                            ),
                           ),
                         ),
                       ],
-                    );
-                  },
-                ),
-              ),
-            ],
-          );
-        }),
-      ),
-    );
+                    ),
+                  ),
+                ],
+              );
+            },
+          ),
+        ),
+      ],
+    )
+        // }),
+        );
   }
 }
--- lib/utils/color_palette.dart
+++ lib/utils/color_palette.dart
@@ -12,12 +12,13 @@ class ColorPalette {
 
   ///grey
   Color get grey2F2 => _isLightTheme ? const Color(0xFFF2F2F2) : const Color(0xFFF2F2F2);
-  Color get grey3F3 => const Color.fromRGBO(243, 243, 243, 1);
-  Color get grey3F => const Color.fromRGBO(233, 233, 233, 1);
+  Color get grey3F3 => const Color(0xFFF3F3F3);
+  Color get grey9E9 => const Color(0xFFE9E9E9);
 
   ///blue
   Color get blueD4B => const Color(0xFF032D4B);
   Color get blue1FC => const Color(0xFFE2F1FC);
+  Color get blue1AD4B => const Color(0x1A032D4B);
 
   ///red
   Color get red000 => const Color(0xFFCC0000);
@@ -25,11 +26,12 @@ class ColorPalette {
   ///text
   Color get text444 => const Color(0xFF444444);
   Color get text666 => const Color(0xFF666666);
+  Color get text97 => const Color(0xFF979797);
 
   ///common
   Color get white => Colors.white;
   Color get black => Colors.black;
 
   //yellow
-  Color get mainYellow => const Color.fromRGBO(247, 170, 5, 1);
+  Color get yellowF7A => const Color(0xFFF7AA05);
 }
