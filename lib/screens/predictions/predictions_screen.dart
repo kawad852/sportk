3386038441ac80a07f52b05093bd6e2c512c57b7
Almhyar Zahra <almhyarzahra@gmail.com>
2024-03-22 07:06:20 -0500

    create prediction

--- lib/screens/predictions/predictions_screen.dart
+++ lib/screens/predictions/predictions_screen.dart
@@ -1,5 +1,6 @@
 import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:sportk/alerts/feedback/app_feedback.dart';
 import 'package:sportk/model/match_points_model.dart';
 import 'package:sportk/model/prediction_model.dart';
 import 'package:sportk/network/api_service.dart';
@@ -25,32 +26,40 @@ class _PredictionsScreenState extends State<PredictionsScreen> {
   int _selectedFirstScore = 0;
   bool _visibleResult = false;
   bool _visibleFirstScore = false;
+  int _homeScore = 1;
+  int _awayScore = 1;
+  late String _firstScoreId;
+  late String _prediction;
 
   PointsData get pointData => widget.pointsData;
 
   late CommonProvider _commonProvider;
 
-  // void createPrediction() {
-  //   ApiFutureBuilder<PredictionModel>().fetch(
-  //     context,
-  //     future: () async {
-  //       final prediction = _commonProvider.createPrediction(matchId: matchId, homeScore: homeScore, awayScore: awayScore, firstScoreId: firstScoreId, prediction: prediction);
-  //       return prediction;
-  //     },
-  //     onComplete: (snapshot) {
-  //       // if (snapshot.data!.status == 1) {
-  //       //   context.push(PredictionsScreen(pointsData: snapshot.data!));
-  //       // } else {
-  //       //   context.showSnackBar(context.appLocalization.cantPredictMatch);
-  //       // }
-  //     },
-  //   );
-  // }
+  void createPrediction() {
+    ApiFutureBuilder<PredictionModel>().fetch(
+      context,
+      future: () async {
+        final prediction = _commonProvider.createPrediction(
+          matchId: pointData.matchId!,
+          homeScore: _homeScore.toString(),
+          awayScore: _awayScore.toString(),
+          firstScoreId: _firstScoreId,
+          prediction: _prediction,
+        );
+        return prediction;
+      },
+      onComplete: (snapshot) {
+        context.showSnackBar(context.appLocalization.successPrediction);
+      },
+    );
+  }
 
   @override
   void initState() {
     super.initState();
     _commonProvider = context.commonProvider;
+    _firstScoreId = pointData.homeId!;
+    _prediction = pointData.homeId!;
   }
 
   @override
@@ -60,7 +69,9 @@ class _PredictionsScreenState extends State<PredictionsScreen> {
         visible: _visibleFirstScore,
         child: ZoomIn(
           child: StretchedButton(
-            onPressed: () {},
+            onPressed: () {
+              createPrediction();
+            },
             margin: const EdgeInsets.symmetric(horizontal: 5),
             child: Text(context.appLocalization.confirm),
           ),
@@ -109,6 +120,14 @@ class _PredictionsScreenState extends State<PredictionsScreen> {
                             if (!_visibleResult) {
                               _visibleResult = true;
                             }
+                            switch (index) {
+                              case 0:
+                                _prediction = pointData.homeId!;
+                              case 1:
+                                _prediction = "draw";
+                              case 2:
+                                _prediction = pointData.awayId!;
+                            }
                           });
                         },
                         child: PredictionsContainer(
@@ -149,6 +168,7 @@ class _PredictionsScreenState extends State<PredictionsScreen> {
                               if (!_visibleFirstScore) {
                                 _visibleFirstScore = true;
                               }
+                              _homeScore = value;
                             });
                           }),
                           VerticalDivider(
@@ -162,6 +182,7 @@ class _PredictionsScreenState extends State<PredictionsScreen> {
                               if (!_visibleFirstScore) {
                                 _visibleFirstScore = true;
                               }
+                              _awayScore = value;
                             });
                           }),
                           TeamName(name: pointData.awayName!),
@@ -198,6 +219,7 @@ class _PredictionsScreenState extends State<PredictionsScreen> {
                             setState(() {
                               _selectedFirstScore = index;
                             });
+                            _firstScoreId = index == 0 ? pointData.homeId! : pointData.awayId!;
                           },
                           child: Padding(
                             padding: const EdgeInsets.symmetric(horizontal: 30),
