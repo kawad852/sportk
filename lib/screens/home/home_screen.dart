635f0dfe592d4caa7d8b70356bf5d355e8717386
Almhyar Zahra <almhyarzahra@gmail.com>
2024-04-06 10:51:16 -0500

    own goal event

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -336,7 +336,7 @@ class _HomeScreenState extends State<HomeScreen> with AutomaticKeepAliveClientMi
                                               timeAdded = period.timeAdded == null ? 30 : 30 + period.timeAdded!;
                                             }
                                             period.events!.map((event) {
-                                              if (event.typeId == 14 || event.typeId == 16) {
+                                              if (event.typeId == 14 || event.typeId == 16 || event.typeId == 15) {
                                                 goalsTime.add(event.minute!.toDouble());
                                               }
                                             }).toSet();
