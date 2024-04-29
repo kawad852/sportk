55be7d83829b64fa55ba0fefabb906273cf7179d
unknown <almhyarzahar@gmail.com>
2024-04-29 09:16:45 -0700

    refresh wallet

--- lib/screens/wallet/wallet_screen.dart
+++ lib/screens/wallet/wallet_screen.dart
@@ -70,202 +70,210 @@ class _WalletScreenState extends State<WalletScreen> with AutomaticKeepAliveClie
     super.build(context);
     return Scaffold(
       drawer: const ProfileScreen(),
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            pinned: true,
-            centerTitle: true,
-            leading: const MenuButton(),
-            title: Text(context.appLocalization.myWallet),
-          ),
-          SliverPadding(
-            padding: const EdgeInsetsDirectional.symmetric(horizontal: 15, vertical: 10),
-            sliver: SliverToBoxAdapter(
-              child: CustomFutureBuilder(
-                future: _futures,
-                onRetry: () {
-                  setState(() {
-                    _futures = _initializeFutures();
-                  });
-                },
-                onLoading: () => const ShimmerLoading(child: WalletLoading()),
-                onComplete: (context, snapshot) {
-                  final user = snapshot.data![0] as UserModel;
-                  final points = snapshot.data![1] as PointsModel;
-                  String invitationCodePoints = "";
-                  String adPoints = "";
-                  points.data!.map(
-                    (e) {
-                      if (e.id == 1) {
-                        invitationCodePoints = e.value!;
-                      }
-                      if (e.id == 5) {
-                        adPoints = e.value!;
-                      }
-                    },
-                  ).toSet();
-                  return Column(
-                    children: [
-                      const WalletCard(),
-                      GoogleRewarded(
-                        points: int.parse(adPoints),
-                        onClose: () {},
-                        child: WatchAdButton(
-                          points: adPoints,
-                          onPressed: () {},
+      body: RefreshIndicator(
+        onRefresh: ()async{
+          setState(() {
+           _futures = _initializeFutures();
+           _vexKey.currentState!.refresh();
+          });
+        },
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              pinned: true,
+              centerTitle: true,
+              leading: const MenuButton(),
+              title: Text(context.appLocalization.myWallet),
+            ),
+            SliverPadding(
+              padding: const EdgeInsetsDirectional.symmetric(horizontal: 15, vertical: 10),
+              sliver: SliverToBoxAdapter(
+                child: CustomFutureBuilder(
+                  future: _futures,
+                  onRetry: () {
+                    setState(() {
+                      _futures = _initializeFutures();
+                    });
+                  },
+                  onLoading: () => const ShimmerLoading(child: WalletLoading()),
+                  onComplete: (context, snapshot) {
+                    final user = snapshot.data![0] as UserModel;
+                    final points = snapshot.data![1] as PointsModel;
+                    String invitationCodePoints = "";
+                    String adPoints = "";
+                    points.data!.map(
+                      (e) {
+                        if (e.id == 1) {
+                          invitationCodePoints = e.value!;
+                        }
+                        if (e.id == 5) {
+                          adPoints = e.value!;
+                        }
+                      },
+                    ).toSet();
+                    return Column(
+                      children: [
+                        const WalletCard(),
+                        GoogleRewarded(
+                          points: int.parse(adPoints),
+                          onClose: () {},
+                          child: WatchAdButton(
+                            points: adPoints,
+                            onPressed: () {},
+                          ),
                         ),
-                      ),
-                      Row(
-                        children: [
-                          WalletButton(
-                            icon: MyIcons.moneyTime,
-                            text: context.appLocalization.recordPoints,
-                            onPressed: () {
-                              context.push(const RecordPointsScreen());
-                            },
-                          ),
-                          const SizedBox(
-                            width: 10,
-                          ),
-                          WalletButton(
-                            icon: MyIcons.replacementRequests,
-                            text: context.appLocalization.replacementRequests,
-                            onPressed: () {
-                              context.push(const SwapRequestsScreen());
-                            },
-                          ),
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      Container(
-                        width: double.infinity,
-                        height: 54,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(MyTheme.radiusSecondary),
-                          color: context.colorPalette.blueF9F,
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        Row(
                           children: [
-                            const CustomSvg(MyIcons.ticketStar),
-                            Padding(
-                              padding: const EdgeInsetsDirectional.only(start: 2, end: 2),
-                              child: Text(
-                                context.appLocalization.invitationCode,
-                                style: TextStyle(
-                                  color: context.colorPalette.blueD4B,
-                                  fontSize: 12,
-                                ),
-                              ),
-                            ),
-                            Container(
-                              width: 170,
-                              height: 30,
-                              alignment: Alignment.center,
-                              decoration: BoxDecoration(
-                                color: context.colorPalette.walletContainerColor,
-                                borderRadius: BorderRadius.circular(MyTheme.radiusSecondary),
-                              ),
-                              child: Text(
-                                user.data!.invitationCode!,
-                                style: TextStyle(
-                                  color: context.colorPalette.blueD4B,
-                                  fontSize: 12,
-                                ),
-                              ),
-                            ),
-                            IconButton(
+                            WalletButton(
+                              icon: MyIcons.moneyTime,
+                              text: context.appLocalization.recordPoints,
                               onPressed: () {
-                                Clipboard.setData(ClipboardData(text: user.data!.invitationCode!)).then(
-                                  (value) {
-                                    ScaffoldMessenger.of(context).showSnackBar(
-                                      SnackBar(
-                                        content: Text(context.appLocalization.copiedInvitionCode),
-                                      ),
-                                    );
-                                  },
-                                );
+                                context.push(const RecordPointsScreen());
                               },
-                              icon: const CustomSvg(MyIcons.copyCode),
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            WalletButton(
+                              icon: MyIcons.replacementRequests,
+                              text: context.appLocalization.replacementRequests,
+                              onPressed: () {
+                                context.push(const SwapRequestsScreen());
+                              },
                             ),
                           ],
                         ),
-                      ),
-                      ShareAppText(points: invitationCodePoints),
-                      const SizedBox(
-                        height: 15,
-                      ),
-                      Row(
-                        children: [
-                          const CustomSvg(MyIcons.coupons),
-                          const SizedBox(
-                            width: 10,
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                          width: double.infinity,
+                          height: 54,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(MyTheme.radiusSecondary),
+                            color: context.colorPalette.blueF9F,
                           ),
-                          Text(
-                            context.appLocalization.vouchers,
-                            style: TextStyle(
-                              color: context.colorPalette.blueD4B,
-                              fontWeight: FontWeight.w600,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              const CustomSvg(MyIcons.ticketStar),
+                              Padding(
+                                padding: const EdgeInsetsDirectional.only(start: 2, end: 2),
+                                child: Text(
+                                  context.appLocalization.invitationCode,
+                                  style: TextStyle(
+                                    color: context.colorPalette.blueD4B,
+                                    fontSize: 12,
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                width: 170,
+                                height: 30,
+                                alignment: Alignment.center,
+                                decoration: BoxDecoration(
+                                  color: context.colorPalette.walletContainerColor,
+                                  borderRadius: BorderRadius.circular(MyTheme.radiusSecondary),
+                                ),
+                                child: Text(
+                                  user.data!.invitationCode!,
+                                  style: TextStyle(
+                                    color: context.colorPalette.blueD4B,
+                                    fontSize: 12,
+                                  ),
+                                ),
+                              ),
+                              IconButton(
+                                onPressed: () {
+                                  Clipboard.setData(ClipboardData(text: user.data!.invitationCode!)).then(
+                                    (value) {
+                                      ScaffoldMessenger.of(context).showSnackBar(
+                                        SnackBar(
+                                          content: Text(context.appLocalization.copiedInvitionCode),
+                                        ),
+                                      );
+                                    },
+                                  );
+                                },
+                                icon: const CustomSvg(MyIcons.copyCode),
+                              ),
+                            ],
+                          ),
+                        ),
+                        ShareAppText(points: invitationCodePoints),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        Row(
+                          children: [
+                            const CustomSvg(MyIcons.coupons),
+                            const SizedBox(
+                              width: 10,
                             ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  );
-                },
+                            Text(
+                              context.appLocalization.vouchers,
+                              style: TextStyle(
+                                color: context.colorPalette.blueD4B,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    );
+                  },
+                ),
               ),
             ),
-          ),
-          VexPaginator(
-            key: _vexKey,
-            query: (pageKey) async => _initializeVouchersFuture(pageKey),
-            onFetching: (snapshot) async => snapshot.data!,
-            pageSize: 10,
-            sliver: true,
-            onLoading: () => const ShimmerLoading(child: VouchersLoading()),
-            builder: (context, snapshot) {
-              final vouchers = snapshot.docs as List<VouchersData>;
-              return vouchers.isEmpty
-                  ? SliverFillRemaining(
-                      hasScrollBody: false,
-                      child: Center(
-                        child: Text(
-                          context.appLocalization.noVouchersAvailable,
-                          style: TextStyle(
-                            color: context.colorPalette.blueD4B,
-                            fontWeight: FontWeight.bold,
+            VexPaginator(
+              key: _vexKey,
+              query: (pageKey) async => _initializeVouchersFuture(pageKey),
+              onFetching: (snapshot) async => snapshot.data!,
+              pageSize: 10,
+              sliver: true,
+              onLoading: () => const ShimmerLoading(child: VouchersLoading()),
+              builder: (context, snapshot) {
+                final vouchers = snapshot.docs as List<VouchersData>;
+                return vouchers.isEmpty
+                    ? SliverFillRemaining(
+                        hasScrollBody: false,
+                        child: Center(
+                          child: Text(
+                            context.appLocalization.noVouchersAvailable,
+                            style: TextStyle(
+                              color: context.colorPalette.blueD4B,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ),
-                      ),
-                    )
-                  : SliverPadding(
-                      padding: const EdgeInsets.symmetric(horizontal: 15),
-                      sliver: SliverGrid(
-                        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 2,
+                      )
+                    : SliverPadding(
+                        padding: const EdgeInsets.symmetric(horizontal: 15),
+                        sliver: SliverGrid(
+                          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                          ),
+                          delegate: SliverChildBuilderDelegate(
+                            childCount: snapshot.docs.length + 1,
+                            (context, index) {
+                              if (snapshot.hasMore && index + 1 == snapshot.docs.length) {
+                                snapshot.fetchMore();
+                              }
+        
+                              if (index == snapshot.docs.length) {
+                                return VexLoader(snapshot.isFetchingMore);
+                              }
+        
+                              final element = vouchers[index];
+                              return CouponsCard(vouchersData: element);
+                            },
+                          ),
                         ),
-                        delegate: SliverChildBuilderDelegate(
-                          childCount: snapshot.docs.length + 1,
-                          (context, index) {
-                            if (snapshot.hasMore && index + 1 == snapshot.docs.length) {
-                              snapshot.fetchMore();
-                            }
-
-                            if (index == snapshot.docs.length) {
-                              return VexLoader(snapshot.isFetchingMore);
-                            }
-
-                            final element = vouchers[index];
-                            return CouponsCard(vouchersData: element);
-                          },
-                        ),
-                      ),
-                    );
-            },
-          ),
-        ],
+                      );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
